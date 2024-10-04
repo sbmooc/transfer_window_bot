a398b00003f4e8ab3b30a0a5164977da16b1de9a
oli mc <oliver.monaghan.coombs@prolific.co>
2024-10-04 20:55:58 +0100

    Initial commit

---
+++ test/transfer_window_bot.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TransferWindowBot from '../lib/transfer_window_bot-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/transfer_window_bot-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TransferWindowBot.TransferWindowBotStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
